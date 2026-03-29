e525f9d5b3a1b636bb88b6e7c3e7be24652616f2
Fellipe Costa <fellipe20070305@gmail.com>
2026-03-29 19:03:30 -0300

    Add files via upload

---
+++ catalogo/js/data.js
@@ -0,0 +1,45 @@
+export const categories = [
+    {
+        title: "Mais assistidos",
+        items: [
+            {
+                img: "https://disney.images.edge.bamgrid.com/ripcut-delivery/v2/variant/disney/a01393ab-13bb-415b-a830-4188ec50fb31/compose?aspectRatio=1.78&format=webp&width=1200",
+                 top10: true, badge: "Clássico", badgeColor: "red", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=g6ng8iy-l0U&t=9s"
+            },
+            {
+                img: "https://disney.images.edge.bamgrid.com/ripcut-delivery/v2/variant/disney/b3b5e25b-58d3-4d58-9581-db3293e18f8e/compose?aspectRatio=1.78&format=webp&width=1200",
+                 progress: 0,
+                youtube: "https://www.youtube.com/watch?v=BuvJZGLclAU"
+            },
+            {
+                img: "https://i.ytimg.com/vi/OQgySPQ5M3Y/maxresdefault.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=zckJCxYxn1g"
+            },
+            {
+                img: "https://lumiere-a.akamaihd.net/v1/images/tron_c895c573.jpeg?region=0,504,935,525",
+                 progress: 0,
+                youtube: "https://www.youtube.com/watch?v=No8t1WodVNk"
+            },
+        ]
+    },
+    {
+        title: "Séries",
+        items: [
+            { img: "https://cinebuzz.com.br/wp-content/uploads/one_piece_a_serie_adaptacao_da_obra_de_eiichiro_oda_estreia_na_netflix.jpg", top10: true, badge: "Nova temporada", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=YC8SLpnxsL4" },
+            { img: "https://i0.wp.com/1.bp.blogspot.com/-Lg9nBOFydVU/X-UxbXaN2RI/AAAAAAAAgBI/wCfWa-cTb4QpzY8pb1ptyMTnS3gkp9DoQCLcBGAsYHQ/s1280/Cobra%2BKai.jpg?ssl=1", top10: true, youtube: "https://www.youtube.com/watch?v=4i21ty_umRM" },
+            { img: "https://occ-0-8407-2219.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABVjiihK6eBqfOrni5u8TgLzZTnuNjGBHloqZlQJjzj1yoFcH1m_kLdcKWBOujYWUIvTox2_0AN9AjOX62myJpMILRufLuqsoaHH9.jpg?r=558", badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=ylTGhpTmbDU" },
+            { img: "https://m.media-amazon.com/images/S/pv-target-images/5c3e0d6b9ad0460aa7e63bb1e1d56c4b75722a98f467f5aa8d22650c27a71e1b.jpg", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=Uv8bfaxl9j4" },
+        ]
+    },
+    {
+        title: "Para maratonar",
+        items: [
+            { img: "https://upload.wikimedia.org/wikipedia/commons/3/38/Stranger_Things_logo.png", top10: true, youtube: "https://www.youtube.com/watch?v=Wb3kaP580kA" },
+            { img: "https://rollingstone.com.br/wp-content/uploads/2025/07/final-de-round-6-faz-referencia-ao-primeiro-episodio-da-serie-entenda.png", top10: true, badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=6zp3mt5Dd2c" },
+            { img: "https://img4.hulu.com/user/v3/artwork/1138ee62-b9d9-4561-8094-3f7cda4bbd22?base_image_bucket_name=image_manager&base_image=019b7fb8-1c6b-7e13-9d1a-b8f6260de60a&size=1200x630&format=webp&operations=%5B%7B%22gradient_vector%22%3A%22(0%2C0%2C0%2C0.5)%7C(0%2C0%2C0%2C0)%7C(0%2C600)%7C(0%2C240)%22%7D%2C%7B%22overlay%22%3A%7B%22position%22%3A%22SouthEast%7C(30%2C30)%22%2C%22operations%22%3A%5B%7B%22image%22%3A%22image_manager%7Ccf4b2481-5be1-4ea7-9b83-02d365f22063%22%7D%2C%7B%22resize%22%3A%22204x204%7Cmax%22%7D%2C%7B%22extent%22%3A%22204x204%22%7D%5D%7D%7D%2C%5D",
+                 top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=03RBqKiK7Ys" },
+            { img: "https://m.media-amazon.com/images/S/pv-target-images/407e66050936f9b996ea97dff072c22606ded1e5e53bb18ad9b4678ee68b1d93.jpg", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=lnY9FWUTY84" },
+        ]
+    }
+];
